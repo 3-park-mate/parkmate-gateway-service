ff2cae2e5fbb3fa3dddec1a28ab676688bbbc6c7
bomikim <kbm8527@gmail.com>
2025-05-23 23:46:29 +0900

    feat: 공통 API 응답 구현(#4)

---
+++ src/main/java/com/parkmate/gateway_service/common/response/ApiResponse.java
@@ -0,0 +1,18 @@
+package com.parkmate.gateway_service.common.response;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@JsonPropertyOrder({
+        "status", "code", "message"
+})
+@Getter
+@AllArgsConstructor
+public class ApiResponse<T> {
+
+    private Integer status;
+    private Integer code;
+    private String message;
+
+}
